761cf4cf05780314b0d5f259fc360cb7df740d89
Bo Tisdale <bo@rave.digital>
2020-07-23 17:13:39 -0400

    Testing.

--- lib/merit/generators/install_generator.rb
+++ lib/merit/generators/install_generator.rb
@@ -8,7 +8,7 @@ module Merit
 
       desc 'Copy config and rules files'
       def copy_migrations_and_model
-        template 'merit.erb', 'config/initializers/merit.rb'
+        #template 'merit.erb', 'config/initializers/merit.rb'
         template 'merit_badge_rules.erb', 'app/models/merit/badge_rules.rb'
         template 'merit_point_rules.erb', 'app/models/merit/point_rules.rb'
         template 'merit_rank_rules.erb', 'app/models/merit/rank_rules.rb'
